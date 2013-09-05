38865373f305a375e0a4949c939339145336740b
Domhnall <domhnallmurphy@hotmail.com>
2013-09-05 08:13:33 +0100

    Add a :status method to Delayed::Job model that returns a Status.

--- app/models/delayed_job.rb
+++ app/models/delayed_job.rb
@@ -1,8 +1,33 @@
 class Delayed::Job
+  class Status
+    attr_accessor :name
+
+    def initialize(name)
+      self.name = name
+    end
+
+    def to_s
+      self.name
+    end
+
+    def ==(another_status)
+      self.name == another_status.name
+    end
+  end
+
+  PENDING = Status.new('pending').freeze
+  PROCESSING = Status.new('processing').freeze
+  FAILING = Status.new('failing').freeze
+
   def apply_handlers
     DelayedJobAdmin.destroy_handlers.each do |clazz|
       handler = clazz.new(self)
       handler.handle
     end
+  end
+
+  def status
+    return PENDING if self.locked_at.nil?
+    self.failed_at ? FAILING : PROCESSING
   end
 end
--- spec/models/delayed_job_spec.rb
+++ spec/models/delayed_job_spec.rb
@@ -5,9 +5,28 @@ describe Delayed::Job do
     @job = Delayed::Job.new
   end
 
-  [:apply_handlers].each do |method|
+  [:apply_handlers, :status].each do |method|
     it "should respond_to method :#{method}" do
       @job.should respond_to method
+    end
+  end
+
+  describe 'namespaced constants' do
+    %w( pending processing failing ).each do |state|
+      name = "Delayed::Job::#{state.upcase}"
+      describe name do
+        it 'should be defined' do
+          name.constantize.should_not be_nil
+        end
+
+        it 'should be a Delayed::Job::Status' do
+          name.constantize.should be_a Delayed::Job::Status
+        end
+
+        it "should have a string representation of '#{state}'" do
+          name.constantize.to_s.should == state
+        end
+      end
     end
   end
 
@@ -33,6 +52,25 @@ describe Delayed::Job do
         DummyHandler.should_receive(:new).and_return(handler_mock)
         handler_mock.should_receive(:handle).once
         @job.apply_handlers
+      end
+    end
+
+    describe '#status' do
+      it 'should return PENDING if locked_at is nil' do
+        @job.locked_at = nil
+        @job.status.should == Delayed::Job::PENDING
+      end
+
+      it 'should return PROCESSING if locked_at is not nil and failed_at is nil' do
+        @job.locked_at = DateTime.now - 20.minutes
+        @job.failed_at = nil
+        @job.status.should == Delayed::Job::PROCESSING
+      end
+
+      it 'should return FAILING if locked_at is not nil and failed_at is not nil' do
+        @job.locked_at = DateTime.now - 20.minutes
+        @job.failed_at = DateTime.now - 10.minutes
+        @job.status.should == Delayed::Job::FAILING
       end
     end
 
