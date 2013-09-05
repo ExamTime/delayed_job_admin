9fbdcdaafc6607209ce0989e024dabd4dba441d6
Domhnall <domhnallmurphy@hotmail.com>
2013-09-05 17:45:48 +0100

    Adding a default_poll_interval_in_secs as a config value.

--- lib/delayed_job_admin.rb
+++ lib/delayed_job_admin.rb
@@ -3,8 +3,9 @@ require "delayed_job_active_record"
 require "haml"
 
 module DelayedJobAdmin
-  mattr_accessor :destroy_handlers
+  mattr_accessor :destroy_handlers, :default_poll_interval_in_secs
   @@destroy_handlers = nil
+  @@default_poll_interval_in_secs = nil
 
   def self.setup
     yield self
