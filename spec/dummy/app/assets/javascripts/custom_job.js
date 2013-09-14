96c9b4bb173185d70e90f9562206374131bb4208
Domhnall <domhnallmurphy@hotmail.com>
2013-09-14 18:58:08 +0100

    Updating the dummy app to take advantage of the poll_job.js utility.

--- spec/dummy/app/assets/javascripts/custom_job.js
+++ spec/dummy/app/assets/javascripts/custom_job.js
@@ -1,5 +1,7 @@
 var CUSTOM_JOB = (function(cj, $) {
 
+  var $status_message = null;
+
   var submit_job = function() {
     $.ajax({
       url: '/custom_jobs',
@@ -7,24 +9,28 @@ var CUSTOM_JOB = (function(cj, $) {
       data: $('#create_custom_job_form').serialize(),
       success: function(data) {
         var job_id = data.job_id;
-        alert('Job ID ' + job_id + ' was successfully submitted');
-        DELAYED_JOB_ADMIN.poll_job(job_id, on_job_success, on_job_failure);
+        $status_message.html('Job ID ' + job_id + ' has been queued');
+        DELAYED_JOB_ADMIN.poll_job(job_id, on_job_success, on_job_failing, on_job_processing, 2);
       },
       dataType: 'json'
     });
   };
 
-  var on_job_success = function() {
-    alert('Job was successfully completed');
-    //alert('Job with ID ' + data.job_id + 'was successfully completed');
+  var on_job_success = function(data) {
+    $status_message.html('Job ID ' + data.job_id + ' has been successfully completed.');
   }
 
-  var on_job_failure = function() {
-    alert('Job failed');
+  var on_job_failing = function(data) {
+    $status_message.html('Job ID ' + data.job_id + ' is failing.');
+  };
+
+  var on_job_processing = function(data) {
+    $status_message.html('Job ID ' + data.job_id + ' is currently being processed ...');
   };
 
   cj.init = function() {
     $('#submit_custom_job').click(submit_job);
+    $status_message = $('#status_message');
   };
 
   return cj;
--- spec/dummy/app/controllers/custom_jobs_controller.rb
+++ spec/dummy/app/controllers/custom_jobs_controller.rb
@@ -3,13 +3,12 @@ class CustomJobsController < ApplicationController
   end
 
   def create
-    binding.pry
     name = params.fetch(:name)
     delay = params.fetch(:delay).to_i
     job = DummyModel.create(name: name, delay_in_seconds: delay).delay.method_to_queue(params[:message])
     respond_to do |format|
       format.html { redirect_to delayed_job_admin.jobs_url }
-      format.json { render json: {job_id: job.id}.to }
+      format.json { render json: {job_id: job.id}.to_json }
     end
   end
 end
