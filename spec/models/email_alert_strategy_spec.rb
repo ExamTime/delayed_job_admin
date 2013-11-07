7a1d078fa55ed255b8426e450bc5f253e3b662f7
Domhnall <domhnallmurphy@hotmail.com>
2013-11-07 16:03:40 +0000

    Filling out specs for email_alert_strategy.

--- spec/models/email_alert_strategy_spec.rb
+++ spec/models/email_alert_strategy_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe DelayedJobAdmin::EmailAlertStrategy do
-  let(:dummy_alert) { double('alert') }
-  let(:instance){ DelayedJobAdmin::EmailAlertStrategy.new(dummy_alert, { emails: 'joe@example.com' }) }
+  let(:dummy_alert) { DelayedJobAdmin::QueueAlert.new('Dummy queue', 10, 5) }
+  let(:emails) { ['joe@example.com', 'zoe@example.com'] }
+  let(:instance){ DelayedJobAdmin::EmailAlertStrategy.new(dummy_alert, { emails: emails }) }
 
   it 'should be defined' do
     DelayedJobAdmin::EmailAlertStrategy.should_not be_nil
@@ -16,13 +17,23 @@ describe DelayedJobAdmin::EmailAlertStrategy do
 
   describe 'instantiation' do
     it 'should be possible when all required arguments are supplied' do
+      instance.should_not be_nil
     end
 
+    it 'should raise an ArgumentError if a queue alert is not supplied' do
+      lambda{ DelayedJobAdmin::EmailAlertStrategy.new(nil, {}) }.should raise_error ArgumentError
+    end
+
+    it 'should raise an ArgumentError if the options hash does not include an :emails key' do
+      lambda{ DelayedJobAdmin::EmailAlertStrategy.new('dummy', {emailer: emails}) }.should raise_error ArgumentError
+    end
   end
 
   describe 'instance method' do
     describe '#alert' do
-      it 'should invoke the #send_alert method on the QueueThresholdAlertMailer' do
+      it 'should invoke the #send_alert method on the DelayedJobAdmin::QueueThresholdAlertMailer' do
+        DelayedJobAdmin::QueueThresholdAlertMailer.should_receive(:send_alert).with(dummy_alert, emails).and_call_original
+        instance.alert
       end
     end
   end
