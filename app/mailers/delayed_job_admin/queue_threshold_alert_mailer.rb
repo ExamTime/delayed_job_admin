37044a6b3ce0a684de68f4d47c013ae87d489452
Domhnall <domhnallmurphy@hotmail.com>
2013-11-07 15:14:50 +0000

    Adding a text email for QueueThresholdAlertMailer.send_alert.

--- app/mailers/delayed_job_admin/queue_threshold_alert_mailer.rb
+++ app/mailers/delayed_job_admin/queue_threshold_alert_mailer.rb
@@ -1,8 +1,9 @@
 module DelayedJobAdmin
   class QueueThresholdAlertMailer
     def send_alert(alert, emails)
+      @alert = alert
       mail(to: emails,
-           subject: I18n.t('delayed_job_admin.alert_strategies.alert_email.subject',
+           subject: I18n.t('delayed_job_admin.queue_thershold_alert_mailer.send_alert.subject',
                            queue_name: alert.queue_name,
                            threshold: alert.threshold) )
     end
