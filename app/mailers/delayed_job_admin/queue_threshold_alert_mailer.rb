edbb45698fbf70501f45dae85b2b4cebf346b868
Domhnall <domhnallmurphy@hotmail.com>
2013-11-26 14:53:06 +0000

    Fix typo in mailer subject field.

--- app/mailers/delayed_job_admin/queue_threshold_alert_mailer.rb
+++ app/mailers/delayed_job_admin/queue_threshold_alert_mailer.rb
@@ -3,7 +3,7 @@ module DelayedJobAdmin
     def send_alert(alert, emails)
       @alert = alert
       mail(to: emails,
-           subject: I18n.t('delayed_job_admin.queue_thershold_alert_mailer.send_alert.subject',
+           subject: I18n.t('delayed_job_admin.queue_threshold_alert_mailer.send_alert.subject',
                            queue_name: alert.queue_name,
                            threshold: alert.queue_threshold) )
     end
