fb44a45cbadcfc5c817f538d98292495bd8c84fa
Domhnall <domhnallmurphy@hotmail.com>
2013-11-29 11:54:08 +0000

    Adding engine asset precompilation to engine.

--- lib/delayed_job_admin/engine.rb
+++ lib/delayed_job_admin/engine.rb
@@ -2,6 +2,11 @@ module DelayedJobAdmin
   class Engine < ::Rails::Engine
     isolate_namespace DelayedJobAdmin
 
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w{ delayed_job_admin/jobs.css }
+      Rails.application.config.assets.precompile += %w{ delayed_job_admin/poll_job.js delayed_job_admin/jobs.js }
+    end
+
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
