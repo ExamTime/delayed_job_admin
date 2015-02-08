5c78bb99e889b026bc4d1edb7e0cefe5f8170cb5
Domhnall <domhnallmurphy@hotmail.com>
2015-02-08 14:21:37 +0000

    Fixing a bug in the logic for applying blacklisted job queues.

--- app/controllers/delayed_job_admin/accessible_archived_jobs.rb
+++ app/controllers/delayed_job_admin/accessible_archived_jobs.rb
@@ -4,8 +4,8 @@ module DelayedJobAdmin
       @archive = true
       @jobs = DelayedJobAdmin::ArchivedJob.page(params[:page])
                                           .per(DelayedJobAdmin::pagination_jobs_per_page)
-      if blacklist = DelayedJobAdmin.pagination_excluding_queues
-        @jobs = DelayedJobAdmin::ArchivedJob.where("queue IS NULL OR queue NOT IN (?)", blacklist)
+      if( (blacklist = DelayedJobAdmin.pagination_excluding_queues) && !blacklist.blank? )
+        @jobs = @jobs.where("queue IS NULL OR queue NOT IN (?)", blacklist)
       end
 
       render 'delayed_job_admin/shared/index'
--- app/controllers/delayed_job_admin/accessible_jobs.rb
+++ app/controllers/delayed_job_admin/accessible_jobs.rb
@@ -3,8 +3,8 @@ module DelayedJobAdmin
     def index
       @jobs = Delayed::Job.page(params[:page])
                           .per(DelayedJobAdmin::pagination_jobs_per_page)
-      if blacklist = DelayedJobAdmin.pagination_excluding_queues
-        @jobs = Delayed::Job.where("queue IS NULL OR queue NOT IN (?)", blacklist)
+      if( (blacklist = DelayedJobAdmin.pagination_excluding_queues) && !blacklist.blank? )
+        @jobs = @jobs.where("queue IS NULL OR queue NOT IN (?)", blacklist)
       end
 
       render 'delayed_job_admin/shared/index'
